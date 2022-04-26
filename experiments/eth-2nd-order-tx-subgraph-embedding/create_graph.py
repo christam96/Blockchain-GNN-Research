637e3567093b43ff72d8704ae09a75c1312187dc
Chris Tam <chris-tam8@hotmail.com>
2022-04-26 12:23:29 -0700

    Experiments - `create_graph.py` use glob

--- experiments/eth-2nd-order-tx-subgraph-embedding/create_graph.py
+++ experiments/eth-2nd-order-tx-subgraph-embedding/create_graph.py
@@ -1,9 +1,10 @@
+import glob
 import networkx as nx
 import pandas as pd
 
-df = pd.read_csv('/Users/chris/Documents/Research/data/[DATA] 2nd-order transaction network of phishing nodes/Non-phishing/Non-phishing first-order nodes/0x0000000000000000000000000000000000000000.csv')
-Graphtype = nx.DiGraph()
-G = nx.from_pandas_edgelist(df, source='From', target='To', edge_attr='Value', create_using=Graphtype)
+# List of all files in 1st-order node directory
+DATA_BASE_PATH = "/Users/chris/Documents/Research/data/2nd-order transaction network of phishing nodes/"
+files = glob.glob(DATA_BASE_PATH + 'Non-phishing/Non-phishing first-order nodes/*.csv')
 
 print(G.edges.data())
 
