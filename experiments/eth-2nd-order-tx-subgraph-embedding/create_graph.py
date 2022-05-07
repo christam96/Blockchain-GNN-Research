12baa21545121ea118d68bd6ddc0eab92b4a0f0e
Chris Tam <chris-tam8@hotmail.com>
2022-05-07 16:31:21 -0700

    `create_graph()`: Return G if successfull, 0 otherwise

--- experiments/eth-2nd-order-tx-subgraph-embedding/create_graph.py
+++ experiments/eth-2nd-order-tx-subgraph-embedding/create_graph.py
@@ -83,6 +83,7 @@ def create_graph(dataset, root):
             print('  ✅ SUCCESS → G\': {}'.format(G_prime))
             c_success += 1
             G = G_prime
+            return G
         except Exception as e:
             num_incorrect += 1
             print('  ❌ GRAPH MISMATCH')
@@ -94,6 +95,7 @@ def create_graph(dataset, root):
                 highest_node_diff = node_diff
             if edge_diff > highest_edge_diff:
                 highest_edge_diff = edge_diff
+            return 0
 
     print('------------------------------------------')
     num_correct = count-num_incorrect
