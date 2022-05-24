686248e6ce3f5565e3c2af47190ff48eada954a9
Chris Tam <chris-tam8@hotmail.com>
2022-05-24 12:00:06 -0700

    Graph2Vec: Dimensionality embedding of 256 (used in Graph2Vec paper)

--- experiments/eth-2nd-order-tx-subgraph-embedding/graph2vec.py
+++ experiments/eth-2nd-order-tx-subgraph-embedding/graph2vec.py
@@ -5,7 +5,7 @@ unpickle = open('p-subgraphs.pickle', 'rb')
 subgraphs = pickle.load(unpickle)
 print(len(subgraphs))
 
-model = Graph2Vec(wl_iterations=20)
+model = Graph2Vec(wl_iterations=20, dimensions=256)
 fit = model.fit(subgraphs)
 embedding = model.get_embedding()
 print(embedding.shape)
