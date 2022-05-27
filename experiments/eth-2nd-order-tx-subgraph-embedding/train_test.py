ab6e541826e07da4e14dc8ba8ad47b7ef35fedeb
Chris Tam <chris-tam8@hotmail.com>
2022-05-27 16:01:59 -0700

    Label embeddings: Clean up label assignment and train/test set creation

--- experiments/eth-2nd-order-tx-subgraph-embedding/train_test.py
+++ experiments/eth-2nd-order-tx-subgraph-embedding/train_test.py
@@ -8,28 +8,18 @@ import numpy as np
 # Phishing embeddings
 unpickle = open('p-embeddings.pickle', 'rb')
 p_embeddings = pickle.load(unpickle)
-
-X_y = np.append(p_embeddings, np.ones((len(p_embeddings),1)), axis=1)
-X = X_y[:,:-1]
-y = X_y[:,-1]
-pX_train, pX_test, py_train, py_test = train_test_split(X, y, test_size=0.25, random_state=42)
+p_X_y = np.append(p_embeddings, np.ones((len(p_embeddings),1)), axis=1)
 
 
 # Non-Phishing embeddings
 unpickle = open('np-embeddings.pickle', 'rb')
 np_embeddings = pickle.load(unpickle)
+np_X_y = np.append(np_embeddings, np.zeros((len(np_embeddings),1)), axis=1)
 
-X_y = np.append(np_embeddings, np.zeros((len(np_embeddings),1)), axis=1)
-X = X_y[:,:-1]
-y = X_y[:,-1]
-npX_train, npX_test, npy_train, npy_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X = np.concatenate((p_X_y[:,:-1], np_X_y[:,:-1]))
+y = np.concatenate((p_X_y[:,-1], np_X_y[:,-1]))
 
-
-# Stack X and y
-X_train = np.concatenate((pX_train, npX_train))
-y_train = np.concatenate((py_train, npy_train))
-X_test = np.concatenate((pX_test, npX_test))
-y_test = np.concatenate((py_test, npy_test))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.99)
 
 print(X_train.shape)
 print(y_train.shape)
