dd6b4d03740e8dd810bb9287b9e4aefbbb6cdd67
Chris Tam <chris-tam8@hotmail.com>
2022-05-28 17:19:39 -0700

    Graph2Vec: Refactor to loop through phishing/non-phishing subgraphs

--- experiments/eth-2nd-order-tx-subgraph-embedding/graph2vec.py
+++ experiments/eth-2nd-order-tx-subgraph-embedding/graph2vec.py
@@ -1,14 +1,16 @@
 from karateclub import Graph2Vec
 import pickle
 
-unpickle = open('p-subgraphs.pickle', 'rb')
-subgraphs = pickle.load(unpickle)
-print(len(subgraphs))
+subgraphs = ['p', 'np']
+for sg in subgraphs:
+    unpickle = open('{}-subgraphs.pickle'.format(sg), 'rb')
+    subgraphs = pickle.load(unpickle)
+    print(len(subgraphs))
 
-model = Graph2Vec(wl_iterations=20, dimensions=256)
-fit = model.fit(subgraphs)
-embedding = model.get_embedding()
-print(embedding.shape)
+    model = Graph2Vec(wl_iterations=10, dimensions=256)
+    fit = model.fit(subgraphs)
+    embedding = model.get_embedding()
+    print(embedding.shape)
 
-with open('p-embeddings.pickle', 'wb') as fh:
-    pickle.dump(embedding, fh)
+    with open('{}-embeddings.pickle'.format(sg), 'wb') as fh:
+        pickle.dump(embedding, fh)
