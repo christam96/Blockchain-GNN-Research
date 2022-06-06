df374c60c9c62f85b018e92632edee80ec0e3adc
Chris Tam <chris-tam8@hotmail.com>
2022-06-06 14:03:17 -0700

    XBlock-ETH [Clustering]: Commit `kmeans_clustering.py`

---
+++ experiments/XBlock-ETH/kmeans_clustering.py
@@ -0,0 +1,8 @@
+from sklearn.cluster import KMeans
+import pickle
+
+##### Cluster subgraph embeddings using k-means clustering
+# In: 2D matrix of subgraphs embeddings
+# Compute: Cluster using k-means clustering
+# Out: Fitted KMeans model
+#####
