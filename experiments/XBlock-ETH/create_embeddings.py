ddba9570a68b68d88dfb7771e12bd1317dd29e76
Chris Tam <chris-tam8@hotmail.com>
2022-06-06 13:03:24 -0700

    XBlock-ETH [Embeddings]: Commit `create_embeddings.py`

---
+++ experiments/XBlock-ETH/create_embeddings.py
@@ -0,0 +1,9 @@
+from karateclub import Graph2Vec
+import pickle
+
+##### Create subgraph embeddings
+# In: List of transaction subgraphs
+# Compute: Use embedding model of choice
+#    * graph2vec
+# Out: Subgraph embeddings
+#####
