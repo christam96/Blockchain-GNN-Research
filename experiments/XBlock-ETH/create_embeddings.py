1250bbe80451bc8bb3062141ec15aacb4dad450e
Chris Tam <chris-tam8@hotmail.com>
2022-06-06 13:32:11 -0700

    XBlock-ETH [Embeddings]: Create subgraph embeddings using `graph2vec`

--- experiments/XBlock-ETH/create_embeddings.py
+++ experiments/XBlock-ETH/create_embeddings.py
@@ -7,3 +7,15 @@ import pickle
 #    * graph2vec
 # Out: Subgraph embeddings
 #####
+
+unpickle = open('XBlock-subgraphs-{}.pickle'.format('0to999999'), 'rb')
+subgraphs = pickle.load(unpickle)
+print(len(subgraphs))
+
+model = Graph2Vec(wl_iterations=10, dimensions=256)
+fit = model.fit(subgraphs)
+embedding = model.get_embedding()
+print(embedding.shape)
+
+with open('XBlock-embeddings-{}.pickle'.format('0to999999'), 'wb') as fh:
+    pickle.dump(embedding, fh)
